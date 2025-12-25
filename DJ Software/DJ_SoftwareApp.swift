601e697248d406afc687f67f9f22b2be4823ad80
Nelson Cabrera <nelsoncabrera06@gmail.com>
2025-12-25 10:21:27 +0200

    Initial Commit

---
+++ DJ Software/DJ_SoftwareApp.swift
@@ -0,0 +1,17 @@
+//
+//  DJ_SoftwareApp.swift
+//  DJ Software
+//
+//  Created by Nelson Cabrera on 25.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct DJ_SoftwareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DJ SoftwareTests/DJ_SoftwareTests.swift
@@ -0,0 +1,17 @@
+//
+//  DJ_SoftwareTests.swift
+//  DJ SoftwareTests
+//
+//  Created by Nelson Cabrera on 25.12.2025.
+//
+
+import Testing
+@testable import DJ_Software
+
+struct DJ_SoftwareTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
